3a51e924233b579b15ac510753f6129407ac5b93
Craig Bennett <prefrontal@gmail.com>
2016-01-09 23:36:20 -0800

    Split data off into category

---
+++ VXMetar WatchKit Extension/VXReportingStationManager+Data.h
@@ -0,0 +1,15 @@
+//
+//  VXReportingStationManager.h
+//  VXMetar
+//
+//  Created by Craig Bennett on 12/31/15.
+//  Copyright © 2015 Voxelwise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VXReportingStationManager (Data)
+
+- (NSMutableArray *) loadStations;
+
+@end
